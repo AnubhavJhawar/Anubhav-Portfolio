e1e2382ea17ececbbc4b0c683238ace431e05893
AnubhavJhawar <anubhavjhawar08@gmail.com>
2021-12-10 00:11:11 +0530

    added chat bot

--- projects/script.js
+++ projects/script.js
@@ -87,15 +87,24 @@ getProjects().then(data => {
 // fetch projects end
 
 // Start of Tawk.to Live Chat
-// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-//   (function(){
-//   var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-//   s1.async=true;
-//   s1.src='https://embed.tawk.to/60f70460649e0a0a5ccd22a7/1fb2ei71o';
-//   s1.charset='UTF-8';
-//   s1.setAttribute('crossorigin','*');
-//   s0.parentNode.insertBefore(s1,s0);
-//   })();
+// <!--Start of Tawk.to Script-->
+{/* <script type="text/javascript"> */}
+// <!--Start of Tawk.to Script-->
+{/* <script type="text/javascript"> */}
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/61b23e9980b2296cfdd0f27a/1fmg492d5';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+{/* </script> */}
+{/* <!--End of Tawk.to Script--> */}
+
+// </script>
+// <!--End of Tawk.to Script-->
 // End of Tawk.to Live Chat
 
 // disable developer mode
